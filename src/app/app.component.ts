59324c286e78b5aa83cd1bc4b5ba753f0a88c4f7
Sameer-20l <sameer.shende154@gmail.com>
2024-04-29 16:46:29 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormControl,FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'forms';
+  userLogin(item:any){
+    console.log(item);
+  }
+
+  loginForm=new FormGroup({
+    user :new FormControl(""),
+    password : new FormControl("")
+  })
+  loginUser(){
+    console.log(this.loginForm.value);
+  }
 }
