2362ce82ba09e673b9763e669314c0d0f80dcebe
Sameer-20l <sameer.shende154@gmail.com>
2024-04-30 15:52:21 +0530

    second update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormControl,FormGroup } from '@angular/forms';
+import { FormControl,FormGroup,Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 ;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ReactiveFormsModule],
+  imports: [RouterOutlet,ReactiveFormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,10 +18,13 @@ export class AppComponent {
   }
 
   loginForm=new FormGroup({
-    user :new FormControl(""),
+    user :new FormControl("",[Validators.required,Validators.email]),
     password : new FormControl("")
   })
   loginUser(){
     console.log(this.loginForm.value);
+  }
+  get user(){
+    return this.loginForm.get('user');
   }
 }
